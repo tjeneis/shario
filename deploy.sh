949f8f15f3a07ac0b5f58f6587dc43c6753c5a09
Thomas Neis <thomas.neis@weareyou.com>
2022-01-21 17:26:53 +0100

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:ThomasNeis-WeAreYou/player.git master:gh-pages
+git push -f git@github.com:ThomasNeis-WeAreYou/shario.git master:gh-pages
 
 cd -
